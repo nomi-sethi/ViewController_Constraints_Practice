133d52f05630da9297a1c3b133b06108c0bf374b
Muhammad Nouman Sethi <49169365+nomi-sethi@users.noreply.github.com>
2022-11-16 16:32:33 +0500

    Add files via upload
    Color boxes made in portrait mode w.r.t general resolution.

---
+++ LandscapePracticeCB/LandscapePracticeCB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandscapePracticeCB
+//
+//  Created by MacBook Pro on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
